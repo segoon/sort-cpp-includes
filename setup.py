65c5aa212b9ad0020134a12f7a46c1fe11e1d04f
Vasily Kulikov <segoon@yandex-team.ru>
2022-01-15 22:40:44 +0300

    fix bin

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name='sort-cpp-includes',
     version='0.0.1',
     packages=find_packages(),
-    scripts=['./src/sort_cpp_includes/sort_cpp_includes.py'],
+    entry_points={
+        'console_scripts': [
+            'sort-cpp-includes = sort_cpp_includes.sort_cpp_includes:main'
+        ]
+    }
 )
